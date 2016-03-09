a6edce828b6728812100ec6f54f0eecbba7c01d3
pfeiring <pfeiring@live.com>
2016-03-09 19:03:17 +0100

    Noen ideer rundt mer levende verdener

--- main.lua
+++ main.lua
@@ -32,6 +32,17 @@ settings.DISTANCE_THRESHOLD = 0.00015;
 settings.STEP_SIZE          = 100;
 
 ------------------------------------------------------------------
+
+local NONE = 1;
+local BALOON = 2;
+
+local FLOATING_UP = 1;
+local FLOATING_DOWN = 2;
+
+local LEFT = 1;
+local RIGHT = 2;
+
+------------------------------------------------------------------
 -- Recipe for level 
 --  Starting point
 --	Objects: width, height, source, parallax effect
@@ -42,8 +53,8 @@ settings.STEP_SIZE          = 100;
 local level_recipe = {};
 
 level_recipe.starting_point = {};
-level_recipe.starting_point.x = 0;
-level_recipe.starting_point.y = 400;
+level_recipe.starting_point.x = 800;
+level_recipe.starting_point.y = 00;
 
 ------------------------------------------------------------------
 
@@ -61,14 +72,18 @@ level_recipe.frame.bottom = level_recipe.frame.y + level_recipe.frame.height / 2
 ------------------------------------------------------------------
 
 level_recipe.objects = {};
-level_recipe.objects[1] = {file = 'world_bw.png', width = 3080 * 0.8, height = 1909 * 0.8, x = 0, y = 0 };
+level_recipe.objects[1] = {file = 'world_bw.png', body = NONE, width = 3080 * 0.8, height = 1909 * 0.8, x = 0, y = 0 };
 --level_recipe.objects[1] = {file = 'world.jpeg', width = 2453 * 0.8, height = 1532 * 0.8, x = 950, y = 320 };
+--level_recipe.objects[2] = {file = 'floating.png', body = NONE, movement = FLOATING, width = 696 * 0.8, height = 717 * 0.8, x = -560, y = -470 };
+level_recipe.objects[2] = {file = 'balloon_1.png', body = BALOON, movement = FLOATING_DOWN, direction = LEFT, width = 300 * 0.8, height = 300 * 0.8, x = -760, y = -150 };
+level_recipe.objects[3] = {file = 'balloon_2.png', body = BALOON, movement = FLOATING_UP, direction = LEFT, width = 127 * 0.8, height = 138 * 0.8, x = 930, y = -200 };
+level_recipe.objects[4] = {file = 'balloon_3.png', body = BALOON, movement = FLOATING_DOWN, direction = LEFT, width = 151 * 0.8, height = 173 * 0.8, x = 1000, y = -220 };
 
 level_recipe.portals = {};
 
 level_recipe.treasures = {};
-level_recipe.treasures[1] = {x = 700,  y = 500};
-level_recipe.treasures[2] = {x = 1000, y = 500};
+level_recipe.treasures[1] = {x = 0,  y = 100};
+level_recipe.treasures[2] = {x = -200, y = 100};
 
 ------------------------------------------------------------------
 -- Master group determines drawing order of groups
@@ -145,6 +160,24 @@ function UI.happy_meter:update(increase)
 end
 
 ------------------------------------------------------------------
+
+local function movement_floating_down(object, random_position_offset, random_time_offset)
+    
+    transition.to(object, {transition = easing.inOutQuad, y = object.y + 20 + random_position_offset, time = 5000 + random_time_offset, onComplete =
+    function()
+        transition.to(object, {transition = easing.inOutQuad, y = object.y - 20 - random_position_offset, time = 5000 + random_time_offset, onComplete = function() movement_floating_down(object, random_position_offset, random_time_offset) end});
+    end});
+end
+
+local function movement_floating_up(object, random_position_offset, random_time_offset)
+    
+    transition.to(object, {transition = easing.inOutQuad, y = object.y - 20 - random_position_offset, time = 5000 + random_time_offset, onComplete =
+    function()
+        transition.to(object, {transition = easing.inOutQuad, y = object.y + 20 + random_position_offset, time = 5000 + random_time_offset, onComplete = function() movement_floating_up(object, random_position_offset, random_time_offset) end});
+    end});
+end
+
+------------------------------------------------------------------
 -- Construct the world
 
 local objects = {};
@@ -157,8 +190,24 @@ for i = 1, #level_recipe.objects do
 	object.x = recipe.x;
 	object.y = recipe.y;
 
+    object.body = recipe.body;
+    object.direction = recipe.direction;
+
 	objects[#objects + 1] = object;
 	objects_group:insert(object);
+
+    if (recipe.movement) then
+
+        local movement = recipe.movement;
+        local random_position_offset = math.random() * 5;
+        local random_time_offset = math.ceil(math.random() * 1000);
+
+        if (movement == FLOATING_DOWN) then
+            movement_floating_down(object, random_position_offset, random_time_offset);
+        elseif (movement == FLOATING_UP) then
+            movement_floating_up(object, random_position_offset, random_time_offset);
+        end
+    end
 end
 
 local treasures = {};
@@ -203,7 +252,7 @@ local previous_location;
 local current_location;
 local delta_location;
 
-local location_handler = function( event )
+local location_handler = function(event)
 
     ------------------------------------------------------------------
     -- Check for error (user may have turned off location services)
@@ -302,7 +351,7 @@ local location_handler = function( event )
 
         if (movement.valid) then
 
-			-- Move objects
+			-- Move player
 
             local step_x = settings.STEP_SIZE * math.cos(movement.direction);
             local step_y = settings.STEP_SIZE * math.sin(movement.direction);
@@ -312,21 +361,44 @@ local location_handler = function( event )
 
             camera:update(player);
 
-			-- Check for portals
+            -- Check
 
+            for i = 1, #objects do
 
+                local object = objects[i];
+
+                if (object.body == BALOON and not object.taken and collides(player, objects[i])) then
+                    
+                    transition.cancel(object);
+
+                    if (object.direction == LEFT) then
+                        transition.to(object, {transition = easing.inOutQuad, time = 40000, y = object.y - 2000, x = object.x + 100 * math.random(-5, 1)});
+                    else
+                        transition.to(object, {transition = easing.inOutQuad, time = 40000, y = object.y - 2000, x = object.x + 100 * math.random(1, 5)});
+                    end
+
+                    object.taken = true;
+                end
+            end
 			
 			-- Check for treasure takings
 		
 			for i = 1, #treasures do
-				if (collides(player, treasures[i]) and not treasures[i].taken) then
+
+                local treasure = treasures[i];
+
+				if (not treasure.taken and collides(player, treasure)) then
 					
-                    treasures[i].isVisible = false;
-                    treasures[i].taken = true;
+                    treasure.isVisible = false;
+                    treasure.taken = true;
 
                     UI.happy_meter:update(0.1);
 				end
 			end
+
+            -- Check for portals
+
+            
         end
     end
 end
