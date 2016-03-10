421a8f72266d2c305ee799653ce4a735c0782f11
pfeiring <pfeiring@live.com>
2016-03-10 18:01:21 +0100

    Test av ballong event.

--- main.lua
+++ main.lua
@@ -32,15 +32,26 @@ settings.DISTANCE_THRESHOLD = 0.00015;
 settings.STEP_SIZE          = 100;
 
 ------------------------------------------------------------------
+-- Enum
 
 local NONE = 1;
 local BALOON = 2;
+local TREASURE = 3;
 
 local FLOATING_UP = 1;
 local FLOATING_DOWN = 2;
 
-local LEFT = 1;
-local RIGHT = 2;
+------------------------------------------------------------------
+-- Utility functions
+
+local value_or_default = function(value, default)
+    
+    if (value) then
+        return value;
+    end
+
+    return default;
+end
 
 ------------------------------------------------------------------
 -- Recipe for level 
@@ -59,8 +70,8 @@ level_recipe.starting_point.y = 00;
 ------------------------------------------------------------------
 
 level_recipe.frame = {};
-level_recipe.frame.width = 3080 * 0.8;
-level_recipe.frame.height = 1909 * 0.8;
+level_recipe.frame.width = 2856 * 0.8;
+level_recipe.frame.height = 2856 * 0.8;
 level_recipe.frame.x = 0;
 level_recipe.frame.y = 0;
 
@@ -71,19 +82,20 @@ level_recipe.frame.bottom = level_recipe.frame.y + level_recipe.frame.height / 2
 
 ------------------------------------------------------------------
 
+local index = 0;
+
 level_recipe.objects = {};
-level_recipe.objects[1] = {file = 'world_bw.png', body = NONE, width = 3080 * 0.8, height = 1909 * 0.8, x = 0, y = 0 };
+level_recipe.objects[index + 1] = {file = 'world_bw.png', body = NONE, width = 2856 * 0.8, height = 2856 * 0.8, x = 0, y = 0 };
 --level_recipe.objects[1] = {file = 'world.jpeg', width = 2453 * 0.8, height = 1532 * 0.8, x = 950, y = 320 };
 --level_recipe.objects[2] = {file = 'floating.png', body = NONE, movement = FLOATING, width = 696 * 0.8, height = 717 * 0.8, x = -560, y = -470 };
-level_recipe.objects[2] = {file = 'balloon_1.png', body = BALOON, movement = FLOATING_DOWN, direction = LEFT, width = 300 * 0.8, height = 300 * 0.8, x = -760, y = -150 };
-level_recipe.objects[3] = {file = 'balloon_2.png', body = BALOON, movement = FLOATING_UP, direction = LEFT, width = 127 * 0.8, height = 138 * 0.8, x = 930, y = -200 };
-level_recipe.objects[4] = {file = 'balloon_3.png', body = BALOON, movement = FLOATING_DOWN, direction = LEFT, width = 151 * 0.8, height = 173 * 0.8, x = 1000, y = -220 };
+level_recipe.objects[index + 2] = {file = 'balloon_1.png', event = {type = BALOON, offset = {x = -100, y = -200}}, width = 300 * 0.8, height = 300 * 0.8, x = -760, y = -150 };
+level_recipe.objects[index + 3] = {file = 'balloon_2.png', event = {type = BALOON, offset = {x = -100, y = -200}}, width = 127 * 0.8, height = 138 * 0.8, x = 930, y = -200 };
+level_recipe.objects[index + 4] = {file = 'balloon_3.png', event = {type = BALOON, offset = {x = -100, y = -200}}, width = 151 * 0.8, height = 173 * 0.8, x = 1000, y = -220 };
 
-level_recipe.portals = {};
+index = index + 4;
 
-level_recipe.treasures = {};
-level_recipe.treasures[1] = {x = 0,  y = 100};
-level_recipe.treasures[2] = {x = -200, y = 100};
+level_recipe.objects[index + 1] = {rectangle = true, body = TREASURE, width = 30, height = 30, fill_color = {1, 1, 0}, x = 0,  y = 100};
+level_recipe.objects[index + 2] = {rectangle = true, body = TREASURE, width = 30, height = 30, fill_color = {1, 1, 0}, x = -200,  y = 100};
 
 ------------------------------------------------------------------
 -- Master group determines drawing order of groups
@@ -160,6 +172,7 @@ function UI.happy_meter:update(increase)
 end
 
 ------------------------------------------------------------------
+-- Movement
 
 local function movement_floating_down(object, random_position_offset, random_time_offset)
     
@@ -178,6 +191,40 @@ local function movement_floating_up(object, random_position_offset, random_time_
 end
 
 ------------------------------------------------------------------
+-- Event
+
+local balloon_event = function(event)
+    
+    local object = event.target;
+
+    if (not object.in_transition) then
+
+        local offset_x = object.offset.x;
+        local offset_y = object.offset.y;
+
+        if (object.transitioned) then
+            
+            offset_x = -offset_x;
+            offset_y = -offset_y;
+
+            object.transitioned = nil;
+        else
+            object.transitioned = true;
+        end
+
+        transition.to(object, { transition = easing.inOutQuad,
+                                time = 8000,
+                                x = object.x + offset_x,
+                                y = object.y + offset_y,
+                                onComplete = function() object.in_transition = nil; end});
+
+        object.in_transition = true;
+    end
+
+    return true;
+end
+
+------------------------------------------------------------------
 -- Construct the world
 
 local objects = {};
@@ -185,7 +232,16 @@ local objects = {};
 for i = 1, #level_recipe.objects do
 	
 	local recipe = level_recipe.objects[i];
-	local object = display.newImageRect(recipe.file, recipe.width, recipe.height);
+	local object;
+
+    -- Asset type
+
+    if (recipe.rectangle) then
+        object = display.newRect(0, 0, recipe.width, recipe.height);
+        object:setFillColor(unpack(recipe.fill_color));
+    else
+        object = display.newImageRect(recipe.file, recipe.width, recipe.height);
+    end
 
 	object.x = recipe.x;
 	object.y = recipe.y;
@@ -195,6 +251,24 @@ for i = 1, #level_recipe.objects do
 
 	objects[#objects + 1] = object;
 	objects_group:insert(object);
+
+    -- Events
+
+    if (recipe.event) then
+        
+        local event = recipe.event;
+
+        if (event.type == BALOON) then
+
+            object.offset = {};
+            object.offset.x = event.offset.x;
+            object.offset.y = event.offset.y;
+            
+            object:addEventListener('tap', balloon_event);
+        end
+    end
+
+    -- Movement
 
     if (recipe.movement) then
 
@@ -210,20 +284,7 @@ for i = 1, #level_recipe.objects do
     end
 end
 
-local treasures = {};
-
-for i = 1, #level_recipe.treasures do
-	
-	local recipe = level_recipe.treasures[i];
-	local treasure = display.newRect(0, 0, 30, 30);
-
-	treasure.x = recipe.x;
-	treasure.y = recipe.y;
-	treasure:setFillColor(1, 1, 0);
-
-	treasures[#treasures + 1] = treasure;
-	objects_group:insert(treasure);
-end
+------------------------------------------------------------------
 
 local player = display.newRect(0, 0, 50, 50);
 player.x = level_recipe.starting_point.x;
@@ -351,8 +412,6 @@ local location_handler = function(event)
 
         if (movement.valid) then
 
-			-- Move player
-
             local step_x = settings.STEP_SIZE * math.cos(movement.direction);
             local step_y = settings.STEP_SIZE * math.sin(movement.direction);
 
@@ -360,45 +419,6 @@ local location_handler = function(event)
             player.y = player.y - step_y;
 
             camera:update(player);
-
-            -- Check
-
-            for i = 1, #objects do
-
-                local object = objects[i];
-
-                if (object.body == BALOON and not object.taken and collides(player, objects[i])) then
-                    
-                    transition.cancel(object);
-
-                    if (object.direction == LEFT) then
-                        transition.to(object, {transition = easing.inOutQuad, time = 40000, y = object.y - 2000, x = object.x + 100 * math.random(-5, 1)});
-                    else
-                        transition.to(object, {transition = easing.inOutQuad, time = 40000, y = object.y - 2000, x = object.x + 100 * math.random(1, 5)});
-                    end
-
-                    object.taken = true;
-                end
-            end
-			
-			-- Check for treasure takings
-		
-			for i = 1, #treasures do
-
-                local treasure = treasures[i];
-
-				if (not treasure.taken and collides(player, treasure)) then
-					
-                    treasure.isVisible = false;
-                    treasure.taken = true;
-
-                    UI.happy_meter:update(0.1);
-				end
-			end
-
-            -- Check for portals
-
-            
         end
     end
 end
@@ -454,17 +474,14 @@ local debug_handler_event = function(event)
 	
 	debug_handler();
 
-	if (event.phase == 'ended') then
+    local delta_x = event.x - HALF_SCREEN_WIDTH;
+    local delta_y = event.y - HALF_SCREEN_HEIGHT;
 
-        local delta_x = event.x - HALF_SCREEN_WIDTH;
-        local delta_y = event.y - HALF_SCREEN_HEIGHT;
+    local alpha = math.atan2(delta_y, delta_x);
+    local magnitude = delta_x * delta_x + delta_y * delta_y;
 
-        local alpha = math.atan2(delta_y, delta_x);
-        local magnitude = delta_x * delta_x + delta_y * delta_y;
-
-        debug_event.longitude = debug_event.longitude + 0.1 * magnitude * math.cos(alpha);
-        debug_event.latitude = debug_event.latitude - 0.1 * magnitude * math.sin(alpha);
-	end
+    debug_event.longitude = debug_event.longitude + 0.1 * magnitude * math.cos(alpha);
+    debug_event.latitude = debug_event.latitude - 0.1 * magnitude * math.sin(alpha);
 end
 
 ------------------------------------------------------------------
@@ -475,7 +492,7 @@ camera:update(player);
 if (settings.DEBUG_WITH_DATA) then
     timer.performWithDelay(50, debug_handler_data, -1);
 elseif (settings.DEBUG_WITH_EVENT) then
-	debug_with_event_detector:addEventListener('touch', debug_handler_event);
+	debug_with_event_detector:addEventListener('tap', debug_handler_event);
 else
     Runtime:addEventListener('location', location_handler);
 end
